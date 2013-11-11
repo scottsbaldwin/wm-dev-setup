72decae551bca5a15e5b228397d5b4dd8aa4e9df
Scott Baldwin <scott@wellmatchhealth.com>
2013-11-11 16:05:45 -0600

    Adding note about generating keys and running dotfiles.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@
 ./steps/brews.sh
 
 echo "Don't forget to generate an SSH key and add it to your GitHub account!"
+echo "You can generate a key with: ./steps/key.sh"
+echo "Then, you can also run ./steps.dotfiles.sh to setup your dotfiles"
