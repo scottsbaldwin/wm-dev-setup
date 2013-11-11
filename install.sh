ae3ac0d7470ceabb6ccc8be16a8c49c0505a730e
Scott Baldwin <scott@wellmatchhealth.com>
2013-11-11 17:02:24 -0600

    Fixing typo in path

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ cp files/.gitconfig.local $HOME
 
 echo "Don't forget to generate an SSH key and add it to your GitHub account!"
 echo "You can generate a key with: ./steps/key.sh"
-echo "Then, you can also run ./steps.dotfiles.sh to setup your dotfiles"
+echo "Then, you can also run ./steps/dotfiles.sh to setup your dotfiles"
